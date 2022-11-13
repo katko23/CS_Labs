32dea49e4b90b9fca0f4972911b1ac201d6d4047
katko23 <78986470+katko23@users.noreply.github.com>
2022-11-14 00:14:35 +0200

    Nout (#5)
    * Project Refactoring
    
    * All the files and aditionions for lab IV

---
+++ Hashing/TestLab4.java
@@ -0,0 +1,67 @@
+package CS.Hashing;
+import CS.Asymmetric_Cipher.RSAUtil;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class TestLab4 {
+    public static void main (String[] args) throws NoSuchAlgorithmException {
+        Data sha = new Data();
+        DataBase db = new DataBase();
+
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter Password");
+
+        String userPass = myObj.nextLine();  // Read user input
+        System.out.println("Pass is: " + userPass);  // Output user input
+
+        String password = userPass;
+
+        String publicKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgFGVfrY4jQSoZQWWygZ83roKXWD4YeT2x2p41dGkPixe73rT2IW04glagN2vgoZoHuOPqa5and6kAmK2ujmCHu6D1auJhE2tXP+yLkpSiYMQucDKmCsWMnW9XlC5K7OSL77TXXcfvTvyZcjObEz6LIBRzs6+FqpFbUO9SJEfh6wIDAQAB";
+        String privateKey = "MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm";
+
+        try {
+            //encription
+            String encryptedString = Base64.getEncoder().encodeToString(RSAUtil.encrypt(password, publicKey));
+            System.out.println(encryptedString);
+
+            // hashing
+            byte[] check = sha.Sha(password);
+            System.out.println(check);
+            String s = check.toString();
+            db.passwords.add(s);
+
+            //decription
+            String decryptedString = RSAUtil.decrypt(encryptedString, privateKey);
+            System.out.println(decryptedString);
+
+            //hashing decriptedPass
+            byte[] check_decr = sha.Sha(decryptedString);
+            String s_decr = check.toString();
+            System.out.println(s_decr);
+
+            //checking db pass with decripted one
+            if (Objects.equals(db.passwords.get(0), s_decr)){
+                System.out.println("The varification has been passed");
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println(e.getMessage());
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+    }
+}
